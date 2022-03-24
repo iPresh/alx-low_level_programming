b91727ab99404b58370dbbe0f9eecce463412984
Presh Amuh <amuhprecious@gmail.com>
2022-03-24 16:51:26 -0700

    my task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+
 /**
-*main - Program entry point
-*Return: 0 on success. Eror code otherwise
-*/
+
+ * main - Program entry point
+
+ *
+
+ * Return: 0 on success. Error code otherwise
+
+ */
 
 int main(void)
-{
-char a = Ã'a';
-int n = 0;
 
-while (n < 26)
 {
-putchar(a);
-a++;
-n++;
+
+	char a = 'a';
+
+	int n = 0;
+
+
+
+	while (n < 26)
+
+	{
+
+		putchar(a);
+
+		a++;
+
+		n++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
 }
-putchar('\n');
-return (0);
-}
+
