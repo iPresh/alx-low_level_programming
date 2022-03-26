6e13f1f363974584f688a0503828eed7882d05ee
Presh Amuh <amuhprecious@gmail.com>
2022-03-25 17:38:20 -0700

    my task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: 0 if success
+ */
+int main(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
