ba0f4032239d7202c6a07a2dc29632df1d109aec
Presh Amuh <amuhprecious@gmail.com>
2022-03-28 16:03:29 -0700

    my retry for 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - Return 1 if c is a letter. lowercase or uppercase
- *
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase*
  *@c: The int to print
  * Return: Always 0
  */
